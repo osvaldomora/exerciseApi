4116a352f70f01094b42da252b7141540cf6f93f
jimenezlav <jivel91@gmail.com>
2021-03-17 21:00:22 -0600

    Create project

---
+++ src/test/java/com/jimenezlav/developers/DevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimenezlav.developers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
